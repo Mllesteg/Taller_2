fd6feb0ea6e0f76669ac682ab6145b91001bc7f3
Alejo <alejohead18@gmail.com>
2025-02-25 17:39:05 -0500

    taller dados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,35 @@
 package org.example;
-import java.util.Random;
-import java.util.Scanner;
+
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        String opa = "piedra";
-        String opb = "papel";
-        String opc = "tijeras";
+//variables
+        int max = 6;
+        int min = 1;
+        int range = max - min + 1;
+        int sumaa = 0;
+        int sumab = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int victoriasconor = 0;
-        int victoriasyarbis = 0;
-        while (victoriasconor < 2 && victoriasyarbis < 2) {
-            System.out.println("Elige una opción:");
-            System.out.println("1. " + opa);
-            System.out.println("2. " + opb);
-            System.out.println("3. " + opc);
+        //generador
+        for (int i = 0; i < 10; i++) {
+            int rand1 = (int) (Math.random() * range) + min;
+            sumaa += rand1;
 
-            int opcion = scanner.nextInt();
-            System.out.println(opcion);
-
-
-            int yarbis = random.nextInt(3) + 1;
-            System.out.println(yarbis);
-
-            if (opcion == yarbis) {
-                System.out.println("Tablas");
-            } else if ((opcion == 1 && yarbis == 3) ||
-                    (opcion == 2 && yarbis == 1) ||
-                    (opcion == 3 && yarbis == 2)) {
-                System.out.println("Perdio yarbis");
-            } else {
-                System.out.println("Ganó yarbis");
-            }
+            System.out.println("dado1:" + rand1);
         }
+
+        for (int z = 0; z < 10; z++) {
+            int rand2 = (int) (Math.random() * range) + min;
+            sumab += rand2;
+            System.out.println("dado2:" + rand2);
+
+
+        }
+        System.out.println("total" + sumaa + "total" + sumab);
+
     }
 }
-
-
 
 
 
